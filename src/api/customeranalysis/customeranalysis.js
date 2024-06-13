b4b6736034e80796399da6d615a44456b6779038
ZhangGuokang <zhangguokang@hxdi.com>
2024-06-13 19:37:20 +0800

    客户管理列表更新

---
+++ src/api/customeranalysis/customeranalysis.js
@@ -0,0 +1,28 @@
+import request from '@/utils/request'
+
+/**
+ * 用于存储和分析的客户信息表分页查询
+ * @param {*} data
+ * @returns
+ */
+export function queryCustomerAnalysis(data) {
+  return request({
+    url: '/customeranalysis/customeranalysis/query',
+    method: 'post',
+    data
+  })
+}
+
+/**
+ * 用于存储和分析的客户信息表新增或修改
+ * @param {*} data
+ * @returns
+ */
+export function saveCustomerAnalysis(data) {
+  return request({
+    url: '/customeranalysis/customeranalysis/query',
+    method: 'post',
+    data
+  })
+}
+
